a34401518793abecc1c1431b22583660d8ec421e
Erik Sombroek <eriksombroek@gmail.com>
2025-01-27 21:38:11 +0100

    fix overlap query

--- demo/samples/sample.mjs
+++ demo/samples/sample.mjs
@@ -74,7 +74,7 @@ export default class Sample{
 		this.m_stepCount++;
 	}
 
-	QueryCallback(shapeId, context)
+	QueryCallback(overlapCallbackResult, context)
 	{
 		const {
 			b2Shape_GetBody,
@@ -82,6 +82,8 @@ export default class Sample{
 			b2Shape_TestPoint,
 			b2BodyType,
 		} = this.box2d;
+
+		const {shapeId} = overlapCallbackResult;
 
 		const bodyId = b2Shape_GetBody( shapeId );
 		const bodyType = b2Body_GetType( bodyId );
@@ -122,7 +124,7 @@ export default class Sample{
 		const queryContext = { point: p, bodyId: null };
 
 		const filter = b2DefaultQueryFilter();
-		b2World_OverlapAABB( this.m_worldId, box, filter, (shapeId) => this.QueryCallback(shapeId, queryContext));
+		b2World_OverlapAABB( this.m_worldId, box, filter, (overlapCallbackResult) => this.QueryCallback(overlapCallbackResult, queryContext));
 		filter.delete();
 
 		box.delete();
