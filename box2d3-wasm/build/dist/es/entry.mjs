45998f2a86b52adbcd8a79a25de43e02a5da2c7b
Erik Sombroek <eriksombroek@gmail.com>
2025-05-04 09:02:43 +0200

    add cross isolation check

--- box2d3-wasm/build/dist/es/entry.mjs
+++ box2d3-wasm/build/dist/es/entry.mjs
@@ -22,7 +22,7 @@ export default async (options, ...args) => {
     const hasSIMD = WebAssembly.validate(new Uint8Array([0,97,115,109,1,0,0,0,1,5,1,96,0,1,123,3,2,1,0,10,10,1,8,0,65,0,253,15,253,98,11]));
     /** @type {{ 'default': (options?: unknown) => MainModule) }} */
     const Box2DModule = await (
-      hasSIMD
+      (hasSIMD && window.crossOriginIsolated)
         ? import('./deluxe/Box2D.deluxe.mjs')
         : import('./compat/Box2D.compat.mjs')
     );
